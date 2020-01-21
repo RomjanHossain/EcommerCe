fc273aba8385c0f5def68c20f9e4000b87808323
RomjanHossain <romjanhossain726526@gmail.com>
2020-01-21 16:14:54 +0600

    static settings complete

--- basic/urls.py
+++ basic/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
@@ -7,3 +10,7 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('regi/', views.register_page, name='regi'),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,4 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', "static_root")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn', "my_media")
+
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
