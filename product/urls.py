3354fe456790b8127e2a290c3c53c276d553af4a
RomjanHossain <romjanhossain726526@gmail.com>
2020-01-23 17:20:26 +0600

    completing slugfield

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('prod/', views.product_list_view, name='prod'),
     path('detail/<pk>', views.product_detail_view,  name='deatil'),
+    path('featured/<pk>', views.featured_view, name='feature'),
+    path('prod/<slug:slug>', views.slug_view, name='slugview'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Product
@@ -16,6 +17,37 @@ def product_list_view(request):
 
 def product_detail_view(request, pk=None, *args, **kwargs):
     instance = get_object_or_404(Product, pk=pk)
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    #     print(instance)
+    # except Product.DoesNotExist:
+    #     print('no product here')
+    #     raise Http404("Product does not Exists")
+    # instance = Product.objects.get(id=pk)
+    # if instance is None:
+    #     raise Http404("Object dones not exists")
+    # return instance
+
+    context = {
+        'object': instance
+    }
+    return render(request, "product/detail.html", context)
+
+
+def featured_view(request, pk=None, *args, **kwargs):
+    try:
+        obj = Product.objects.get(id=pk, featured=True)
+    except Product.DoesNotExist:
+        print('this is not featured')
+        obj = ''
+    context = {
+        'object': obj
+    }
+    return render(request, 'product/detail.html', context)
+
+
+def slug_view(request, slug, *args, **kwargs):
+    instance = get_object_or_404(Product, slug=slug)
     context = {
         'object': instance
     }
